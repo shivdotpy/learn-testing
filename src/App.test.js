f5d1b9c05df75ad1269a306938928d77861d2ab5
Shiv Sharma <shivsharma@IN-FVFHM19RQ05P.local>
2024-03-11 10:15:35 +0530

    test for p tag

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("Check if render text `code by`", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Code by Shiv Sharma/i);
   expect(linkElement).toBeInTheDocument();
 });
