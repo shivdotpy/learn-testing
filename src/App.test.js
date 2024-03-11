c213f7952e1c52d3c67a7832605b45b563d58eb5
Shiv Sharma <shivsharma@IN-FVFHM19RQ05P.local>
2024-03-11 10:31:00 +0530

    test image tag

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Test App component", function () {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("Check if render text `code by`", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Code by Shiv Sharma/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/First React Test Case/i);
+  const title = screen.getByTitle("React Logo");
+  expect(text).toBeInTheDocument();
+  expect(title).toBeInTheDocument;
 });
